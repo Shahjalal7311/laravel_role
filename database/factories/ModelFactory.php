77a23a7e0d3a9018aa886cce4d9b8c31cee273ff
Shahjalal <mdjalal2012@gmail.com>
2018-07-25 16:46:04 +0600

    Update ModelFactory.php

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -16,8 +16,8 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
-        'name' => 'Reivo',
-        'email' => 'dev@reivo.co.jp',
+        'name' => 'admin',
+        'email' => 'admin@gmail.com',
         'password' => $password ?: $password = bcrypt('123456'),
         'remember_token' => str_random(10),
     ];
